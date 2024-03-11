883827c2d8f1f92e3a8735d6b94653e54bbba044
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2024-03-11 18:56:06 -0500

    feat: agrega los diferentes public para funcionar

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+
+import path from "path";
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+     sassOptions: {
+        includePaths: [path.join(process.cwd(), "css")],
+    },
+    trailingSlash: true,
+    devIndicators: {
+        buildActivity: false,
+    },
+    eslint: {
+        ignoreDuringBuilds: false,
+    },
+};
 
 export default nextConfig;
