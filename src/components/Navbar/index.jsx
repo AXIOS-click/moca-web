ea57e5f6f8924b0c213618540b76cc2d9a609446
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2024-03-11 20:07:23 -0500

    feat: agrega elementos donde van

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import Link from "next/link";
 import appData from "../../data/app.json";
-import getSiblings from '../../common/getSiblings'
+import getSiblings from "../../common/getSiblings";
 
 const Navbar = ({ navbarRef, logoRef, logoClass }) => {
   const handleDropdown = (e) => {
@@ -29,127 +29,55 @@ const Navbar = ({ navbarRef, logoRef, logoClass }) => {
   };
 
   return (
-    <>
-      <nav className="navbar change navbar-expand-lg" ref={navbarRef}>
-        <div className="container">
-          <Link href="/demos">
-            <a className={`logo ${logoClass && logoClass}`}>
-              <img src={appData.lightLogo} alt="logo" ref={logoRef} />
-            </a>
-          </Link>
+    <nav className="navbar change navbar-expand-lg" ref={navbarRef}>
+      <div className="container">
+        <Link href="/demos">
+          <a className={`logo ${logoClass && logoClass}`}>
+            <img src={appData.lightLogo} alt="logo" ref={logoRef} />
+          </a>
+        </Link>
 
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            onClick={handleMobileDropdown}
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="icon-bar">
-              <i className="fas fa-bars"></i>
-            </span>
-          </button>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          onClick={handleMobileDropdown}
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="icon-bar">
+            <i className="fas fa-bars"></i>
+          </span>
+        </button>
 
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item dropdown" onClick={handleDropdown}>
-                <span
-                  className="nav-link dropdown-toggle"
-                  data-toggle="dropdown"
-                  role="button"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  Home
-                </span>
-                <div className="dropdown-menu">
-                  <Link href="/home7">
-                    <a className="dropdown-item">Home Default</a>
-                  </Link>
-                  <Link href="/home6">
-                    <a className="dropdown-item">Interior Agency</a>
-                  </Link>
-                  <Link href="/home5">
-                    <a className="dropdown-item">Creative Arch</a>
-                  </Link>
-                  <Link href="/home4">
-                    <a className="dropdown-item">Architecture Studio</a>
-                  </Link>
-                  <Link href="/home1">
-                    <a className="dropdown-item">Home Light</a>
-                  </Link>
-                  <Link href="/home2">
-                    <a className="dropdown-item">Interior Light</a>
-                  </Link>
-                  <Link href="/home3">
-                    <a className="dropdown-item">Architecture Light</a>
-                  </Link>
-                </div>
-              </li>
-              <li className="nav-item">
-                <Link href="/about">
-                  <a className="nav-link">About</a>
-                </Link>
-              </li>
-              <li className="nav-item dropdown" onClick={handleDropdown}>
-                <span
-                  className="nav-link dropdown-toggle"
-                  data-toggle="dropdown"
-                  href="#"
-                  role="button"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  Portfolio
-                </span>
-                <div className="dropdown-menu">
-                  <Link href="/work1">
-                    <a className="dropdown-item">Two Column</a>
-                  </Link>
-                  <Link href="/work2">
-                    <a className="dropdown-item">Three Column</a>
-                  </Link>
-                  <Link href="/work3">
-                    <a className="dropdown-item">Four Column</a>
-                  </Link>
-                  <Link href="/project-details">
-                    <a className="dropdown-item">Single Project</a>
-                  </Link>
-                </div>
-              </li>
-              <li className="nav-item dropdown" onClick={handleDropdown}>
-                <span
-                  className="nav-link dropdown-toggle"
-                  data-toggle="dropdown"
-                  href="#"
-                  role="button"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  Blog
-                </span>
-                <div className="dropdown-menu">
-                  <Link href="/blogs">
-                    <a className="dropdown-item">Blogs</a>
-                  </Link>
-                  <Link href="/blog-details">
-                    <a className="dropdown-item">Post Details</a>
-                  </Link>
-                </div>
-              </li>
-              <li className="nav-item">
-                <Link href="/contact">
-                  <a className="nav-link">Contact</a>
-                </Link>
-              </li>
-            </ul>
-          </div>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link href="/">
+                <p className="nav-link">Home</p>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/about">
+                <p className="nav-link">Services</p>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/about">
+                <p className="nav-link">Portfolio</p>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/about">
+                <p className="nav-link">Work With Us</p>
+              </Link>
+            </li>
+          </ul>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 };
 
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,21 +3,19 @@ import Script from "next/script";
 import Head from "next/head";
 import LoadingScreen from "../components/Loading-Screen/loading-screen";
 import "../styles/globals.css";
-import Cursor from "../components/Cursor";
 import ScrollToTop from "../components/scrollToTop";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Archo</title>
+        <title>MoCa</title>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1"
         />
       </Head>
 
-      <Cursor />
       <LoadingScreen />
       <Component {...pageProps} />
       <ScrollToTop />
